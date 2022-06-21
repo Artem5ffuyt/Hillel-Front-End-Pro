75972c360601e7dc83e387b1fd9569246a9c84c7
Artem Bachynskyi <97476036+Artem5ffuyt@users.noreply.github.com>
2022-06-21 18:45:33 +0300

    test

---
+++ maina.js
@@ -0,0 +1,13 @@
+let naMe = prompt('Введить логін');
+if(naMe == 'admin') {
+    alert('Ваш логін вірний' );
+}else {
+    alert('Не верно!');
+}
+
+let pasWord = prompt('Введить пароль');
+if( pasWord == 'qWeRty123') {
+    alert('Ваш пароль вірний' );
+}else {
+    alert('Не верно!');
+}
