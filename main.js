d92f9f61c4b2287ad6874dc0ab35805e2d2c03c6
Artem Bachynskyi <97476036+Artem5ffuyt@users.noreply.github.com>
2022-06-27 14:35:30 +0300

    Домашка
    Переделал алгоритмі ходов

--- main.js
+++ main.js
@@ -5,22 +5,23 @@ const initialY = + prompt(`Start position y. Max 8,min 0`)
 
 
 if (initialX > 0 && initialX < 9  && initialY) {
-
+    
 }
 
 const moveX = +prompt(`move position x`)
 const moveY = +prompt(`move position y`)
 
-const moveUpLong = (initialX + 2 === moveX) && (initialY - 1 === moveY)  || (initialY + 1 === moveY) || (initialY - 2 === moveY) ||
-                   (initialX + 1 === moveX) && (initialY - 2 === moveY) || (initialY + 2 === moveY) || (initialY - 1 === moveY)
+const moveUpLong = (initialX + 2 === moveX) || (initialX - 1 === moveX) && (initialX + 1 === moveX) || (initialX - 2 === moveX) ||
+                   (initialY + 1 === moveY) || (initialY - 2 === moveY) && (initialY + 2 === moveY) || (initialY - 1 === moveY)
 
-const moveShort = (initialX + 2 === moveX) && (initialY - 1 === moveY) || (initialY + 1 === moveY) || (initialY - 2 === moveY) ||
-                  (initialX + 1 === moveX) && (initialY - 2 === moveY) || (initialY + 2 === moveY) || (initialY - 1 === moveY)
+const moveShort = (initialX - 1 === moveX) || (initialX + 2 === moveX) && (initialX - 1 === moveX) || (initialX - 2 === moveX) ||
+                  (initialY  - 2 === moveY) || (initialY - 1 === moveY) && (initialY + 2 === moveY) || (initialY + 1 === moveY)
 
 
-if (moveUpLong && moveShort === true){
-    alert(`right move`)
-} else{
-    alert(`wrong move`)
+if ( moveX && moveY === true){
+    alert(`write mowe`)
+}    
+ else{
+    alert(`Ход не верный`)
 }
 
