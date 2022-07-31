750bcb6d8cdb03e6bc885457b78d3776748d28e6
Artem Bachynskyi <amaz.good@gmail.com>
2022-07-31 23:06:22 +0300

    Just codding

--- maina.js
+++ maina.js
@@ -1,3 +1,4 @@
+/*
 // -----Learn JS----//
 
 
@@ -12,6 +13,38 @@ let sum =0;
   }  
     
 }
-
+let summ = [1,1,1]
+let summ2 = [5,1,6]
+let summ3 = [1,4,3]
  summa(array);
+ summa(summ);
+ summa(summ2);
+ summa(summ3);
  
+
+ // Оброботчик событий
+  
+ function nameBut(){
+ let counter = 0;
+ counter++;
+ console.log(counter)
+ }
+*/
+
+let counter =0;
+function nameBut(el){
+   counter ++;
+   el.innerHTML = "hi" + counter;
+ 
+  
+  }
+
+  function onInput(el){
+console.log(el.value);
+  }
+
+  const text = document.getElementById(`text`)
+  text.title = "gggg"
+  console.log(text.title)
+  text.style.color = "#red"
+  
