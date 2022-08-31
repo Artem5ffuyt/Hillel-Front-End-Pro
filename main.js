d1c2ca03819fd10c3045bca2bafd0336dfc58fb2
Artem Bachynskyi <amaz.good@gmail.com>
2022-08-31 22:09:33 +0300

    lesson

--- main.js
+++ main.js
@@ -1,43 +1,30 @@
- 
+const animals = [
+	['🐭','mouse','Jerry'],
+	['🐹','hamster','Biscuit'],
+	['🐰','rabbit','Bugs'],
+	['🦊','fox','Mrs. Fox'],
+	['🐻','bear','Paddington']
+];
 
+const food = [
+	['🍎','apple',10],
+	['🍐','pear',12],
+	['🍊','tangerine',15],
+	['🍋','lemon',5],
+	['🍌','banana',7]
+];
 
-
-
-let hero = ['Ivan'];
-    hero = ['Richard'];
-let native = ['York','Of'];
-let destination = ['Poltava','In'];
-
-let rainbow = []
-    rainbow = hero.concat(native, destination).reverse()
-    rainbow.push('Gave','Battle','Vain')
-    rainbow[0] = rainbow[4];
-    rainbow[1] = rainbow[2];
-    rainbow[2] = rainbow[3];
-    rainbow[3] = rainbow[7];
-    rainbow[4] = rainbow[6];
-    rainbow[5] = rainbow[7];
-   
-    rainbow.length = 7;
-
-let colors = [`red`, `orange`, `yellow`, `green`, `blue`, `indigo`, `pink`]
-
-document.write(`<div style = " display: flex;
-                               margin: 10% auto;" >`)
-for( let i = 0; i<rainbow.length; i++){
-    document.write(`<div>
-    <span>${rainbow[i]}</span>
-    <div style  = "background:${colors[i]};
-                   width:30px; height:30px;
-                   border-radius:50%;
-                   margin: 20% auto;
-                   margin-right: 10px;
-                   "></div>
-        </div>`)
-   
-   
+function getInfo(arr){
+    let TRs = []
+for(let i = 0; i<arr.length; i++){
+    TRs.push(`<tr><td>${arr[i]}</td></tr>`)
+}
+return `<table>${TRs.join(``)}</table>`
 }
 
-document.write(`</div>`)
+document.write (getInfo(animals))
+// document.write (getInfo(food,`Food`))
 
-console.log(rainbow)
+
+
+
