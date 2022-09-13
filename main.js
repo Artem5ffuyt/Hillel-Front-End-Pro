cd0bcab5caf1196c70e861e218b7f6e3eac6e946
Artem Bachynskyi <amaz.good@gmail.com>
2022-09-13 22:10:13 +0300

    lessen

--- main.js
+++ main.js
@@ -1,44 +1,227 @@
 
 
 
-const animals = [
-    ['🐭', 'mouse', 'Jerry'],
-    ['🐹', 'hamster', 'Biscuit'],
-    ['🐰', 'rabbit', 'Bugs'],
-    ['🦊', 'fox', 'Mrs. Fox'],
-    ['🐻', 'bear', 'Paddington']
-];
+// const animals = [
+//     ['🐭', 'mouse', 'Jerry'],
+//     ['🐹', 'hamster', 'Biscuit'],
+//     ['🐰', 'rabbit', 'Bugs'],
+//     ['🦊', 'fox', 'Mrs. Fox'],
+//     ['🐻', 'bear', 'Paddington']
+// ];
 
-const food = [
-    ['🍎', 'apple', 10],
-    ['🍐', 'pear', 12],
-    ['🍊', 'tangerine', 15],
-    ['🍋', 'lemon', 5],
-    ['🍌', 'banana', 7]
-];
+// const food = [
+//     ['🍎', 'apple', 10],
+//     ['🍐', 'pear', 12],
+//     ['🍊', 'tangerine', 15],
+//     ['🍋', 'lemon', 5],
+//     ['🍌', 'banana', 7]
+// ];
 
-function getInfo(arr) {
-    let TRs = [];
-    for (let i = 0; i < arr.length; i++) {
+// function getInfo(arr) {
+//     let TRs = [];
+//     for (let i = 0; i < arr.length; i++) {
 
-        let TDs = [];
-        for (let j = 0; j < arr[i].length; j++) {
-            TDs.push(`<td>${arr[i][j]}</td>`);
+//         let TDs = [];
+//         for (let j = 0; j < arr[i].length; j++) {
+//             TDs.push(`<td>${arr[i][j]}</td>`);
 
-        }
+//         }
 
-        TRs.push(`<tr>${TDs.join(``)}</tr>`);   
+//         TRs.push(`<tr>${TDs.join(``)}</tr>`);   
       
-    }
-    console.log(TRs);
+//     }
+//     console.log(TRs);
  
     
 
-    return `<table><caption>Animals info</caption><caption> ${TRs.join(``)} </caption></table>`;
+//     return `<table><caption>Animals info</caption><caption> ${TRs.join(``)} </caption></table>`;
     
+// }
+
+
+// document.write(getInfo(animals, `Animals`));
+// document.write(getInfo(food,`info`));
+
+
+
+
+const sports = [
+    ['🤺','fencing'],
+    ['⛸','figure skating'],
+    ['⛷','skier'],
+    ['🏂','snowboarder'],
+    ['🏌','golfing'],
+    ['🚣','rowing boat'],
+    ['🏊','swimming'],
+    ['🤸','gymnastics'],
+    ['🤾','handball']
+];
+const winners = [
+    ['fencing','🥇','fr'],
+    ['fencing','🥈','it'],
+    ['fencing','🥉','us'],
+
+    ['figure skating','🥇','ca'],
+    ['figure skating','🥈','fr'],
+    ['figure skating','🥉','us'],
+
+    ['skier','🥇','no'],
+    ['skier','🥈','us'],
+    ['skier','🥉','fr'],
+
+    ['snowboarder','🥇','us'],
+    ['snowboarder','🥈','jp'],
+    ['snowboarder','🥉','au'],
+
+    ['golfing','🥇','gb'],
+    ['golfing','🥈','se'],
+    ['golfing','🥉','us'],
+
+    ['rowing boat','🥇','us'],
+    ['rowing boat','🥈','ml'],
+    ['rowing boat','🥉','ro'],
+
+    ['swimming','🥇','us'],
+    ['swimming','🥈','gb'],
+    ['swimming','🥉','au'],
+
+    ['gymnastics','🥇','it'],
+    ['gymnastics','🥈','fr'],
+    ['gymnastics','🥉','ua'],
+
+    ['handball','🥇','dk'],
+    ['handball','🥈','ke'],
+    ['handball','🥉','de'],
+]
+const olimpic =  ['🔵','⚫','🔴','🟡','🟢'];
+
+
+const continents = [
+    ['fr','Europe'],
+    ['it','Europe'],
+    ['us','America'],
+    ['ca','America'],
+    ['no','Europe'],
+    ['jp','Asia'],
+    ['au','Oceania'],
+    ['gb','Europe'],
+    ['se','Europe'],
+    ['ro','Europe'],
+    ['ua','Europe'],
+    ['dk','Europe'],
+    ['de','Europe'],
+    ['ke','Africa'],
+    ['ml','Africa']
+
+]
+
+const flags = [
+    ['fr','🇫🇷'],
+    ['it','🇮🇹'],
+    ['us','🇺🇸'],
+    ['ca','🇨🇦'],
+    ['no','🇳🇴'],
+    ['jp','🇯🇵'],
+    ['au','🇦🇺'],
+    ['gb','🇬🇧'],
+    ['se','🇸🇪'],
+    ['ro','🇷🇴'],
+    ['ua','🇺🇦'],
+    ['dk','🇩🇰'],
+    ['de','🇩🇪'],
+    ['ke','🇰🇪'],
+    ['ml','🇲🇱']
+];
+
+
+const getWinnersBaySport = sport => {
+
+    let sportWinners = winners
+    .filters(winner =>winner[0] === sport);
+
+    return sportWinners;
 }
 
+const getContinentByContry = country =>{
+    let countyInfo = continents.find(item => item === [0]);
+    return countyInfo[1];
+}
 
-document.write(getInfo(animals, `Animals`));
-document.write(getInfo(food,`info`));
+const getFlagbyContry = country =>{
+    let countyInfo = flags.find(item => item[0] === country);
+    return countyInfo[1];
+}
 
+const renderThead = () =>{
+    let THs = olimpic
+    .map(item => `<th>${item}</th>`)
+    .join(` `);
+    return `<thead>
+    <tr>
+         <th></th>
+         ${THs}
+    </tr>
+           </thead>`
+}
+
+const renderTbodi = () => {
+    const TRs = sports
+    .map(sport => {
+        let Europe =[];
+        let America =[];
+        let Asia =[];
+        let Africa =[];
+        let Oceania =[];
+
+        let sportIcon = sport[0];
+        let sportName = sport[1];
+        let currentWinners = getWinnersBaySport(sportName)
+        currentWinners.forEach (winner => {
+            let winnerMedal = winner [1];
+
+        let winnerCountry = winner[2];
+        let winnerContinent = getContinentByContry(winnerCountry);
+        let countryFlsg = getFlagbyContry(winnerCountry);
+        winner = `<div>${countryFlsg} - ${winnerMedal}</div>`
+
+        switch(winnerContinent){
+            case `Europe`:
+            Europe.push(winner);
+            break;
+            case `Africa`:
+            Africa.push(winner);
+            break;
+            case `Asia`:
+            Asia.push(winner);
+            break;
+            case `America`:
+            America.push(winner);
+            break;
+            case `Oceania`:
+            Oceania.push(winner);
+            break;
+        }
+        return `<tr>
+            <td>${sportIcon[0]}</td>
+            <td>${Europe.join(``)}</td>
+            <td>${Africa.join(``)}</td>
+            <td>${America.join(``)}</td>
+            <td>${Asia.join(``)}</td>
+            <td>${Oceania.join(``)}</td>
+            
+        </tr>`
+    
+    })
+    
+    .join(``);
+    return `<tbody>
+    ${TRs}
+    </tbody>`
+
+}}
+
+
+document.write(`<table>
+     ${renderTbodi()}
+     ${renderThead()}
+</table>`)
