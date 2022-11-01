65f36b7bc31c5a8ee930b4d8add07aadf5300841
Artem Bachynskyi <amaz.good@gmail.com>
2022-11-01 21:14:00 +0200

    task 17

--- main.js
+++ main.js
@@ -1,27 +1,17 @@
 
+const element = document.querySelector(`#element`);
 
+const form = document.querySelector(`#form`);
+const elementShape = document.querySelector (`#elementOne`);
+const elementBg = document.querySelector(`#elementTwo`);
 
-const square = document.querySelector(`#square`);
-const getRandomIntInclusive = (min=0, max=255) => {
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        return Math.floor(Math.random() * (max - min + 1) + min);
-    };
-const getRandomColor = () => {
-        let colors = [];
-        for(let i=0; i<=2; i++){
-            colors.push(getRandomIntInclusive());
-        };
-        return `rgb(${colors.join(`,`)})`;
-    };
-setInterval(() => square.style.backgroundColor = getRandomColor(), 500);
-square.style.left = 0;
-square.style.top = 0;
+form.addEventListener(`submit`, e => {
+    e.preventDefault();
 
-const squareMoving = setInterval(() => {
-    square.style.top = getRandomIntInclusive(0, document.body.clientHeight-square.offsetHeight) + `px`;
-    square.style.left = getRandomIntInclusive(0, document.body.clientWidth-square.offsetWidth) + `px`;
-}, 1000);
+    element.className = elementShape.value;
+    element.style.background = elementBg.value;
+})
+
   
 
 
