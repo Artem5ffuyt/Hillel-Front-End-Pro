682cd5a422aa26c68fdf95c5af9b8c936be50759
Artem Bachynskyi <amaz.good@gmail.com>
2022-12-22 23:36:05 +0200

    lesson_3task_1

--- lesson_3_task_1/main.js
+++ lesson_3_task_1/main.js
@@ -1,77 +1,41 @@
-// let captionString = `Food prices for`;
-// let date = new Date(),
-//     day = date.getDate(),
-//     month = date.getMonth()+1,
-//     year = date.getFullYear();
+let captionString = `Food prices for`;
+let date = new Date(),
+    day = date.getDate(),
+    month = date.getMonth()+1,
+    year = date.getFullYear();
 
 
-// const aplle =`🍎`;
-// const applePrice = 10;
-// const appleCount = 3;
-// const appleSalePercent = 2.1;
+const aplle =`🍎`;
+const applePrice = 10;
+const appleCount = 3;
+const appleSalePercent = 2.1;
 
 
-// const orange = `🍊`;
-// const orangePrice = 12;
-// const orangeCount = 2;
-// const orangeSalePercent = 0.72;
+const orange = `🍊`;
+const orangePrice = 12;
+const orangeCount = 2;
+const orangeSalePercent = 0.72;
 
 
-// const kiwi = `🥝`;
-// const kiwiPrice = 15;
-// const kiwiCount = 10;
-// const kiwiCountryPercent = 15;
-
-// const resultApple = applePrice * appleCount - appleSalePercent;
-// const resultOrange = orangePrice * orangeCount - orangeSalePercent;
-// const resultkiwi = kiwiPrice * kiwiCount + kiwiCountryPercent;
-
-// const finPraceFor = `Final price for`;
-
-// const totalResult = resultApple + resultOrange + resultkiwi;
+const kiwi = `🥝`;
+const kiwiPrice = 15;
+const kiwiCount = 10;
+const kiwiCountryPercent = 15;
 
 
-// console.log(`${captionString.replace('=', '-')} ${day }.${month}.${year} `)
+const resultApple = applePrice * appleCount - appleSalePercent;
+const resultOrange = orangePrice * orangeCount - orangeSalePercent;
+const resultkiwi = kiwiPrice * kiwiCount + kiwiCountryPercent;
 
-// console.log(`${finPraceFor} ${appleCount} ${aplle} = ${resultApple.toFixed()} UAH` )
-// console.log(`${finPraceFor} ${orangeCount} ${orange} = ${resultOrange.toFixed()} UAH`)
-// console.log(`${finPraceFor} ${kiwiCount} ${kiwi} = ${resultkiwi.toFixed()} UAH`)
+const finPraceFor = `Final price for`;
 
-// console.log(`Final price for all products = ${totalResult.toFixed()} UAH`);
+const totalResult = resultApple + resultOrange + resultkiwi;
 
-let captionString = `Food prices ≠ <ВИВІД ПОТОЧНОГО ДНЯ>.<ВИВІД ПОТОЧНОГО МІСЯЦЯ>.<ВИВІД ПОТОЧНОГО РОКУ>`,
-    
-    apple = `🍎`,
-    applePrice = 10,
-    appleCount = 3,
-    appleSalePercent = 7,
 
-    orange = `🍊`,
-    orangePrice = 12,
-    orangeCount = 2,
-    orangeSalePercent = 3,
+console.log(`${captionString.replace('=', '-')} ${day }.${month}.${year} `)
 
-    kiwi = `🥝`,
-    kiwiPrice = 15,
-    kiwiCount = 10,
-    kiwiCountryPercent = 10,
+console.log(`${finPraceFor} ${appleCount} ${aplle} = ${resultApple.toFixed()} UAH` )
+console.log(`${finPraceFor} ${orangeCount} ${orange} = ${resultOrange.toFixed()} UAH`)
+console.log(`${finPraceFor} ${kiwiCount} ${kiwi} = ${resultkiwi.toFixed()} UAH`)
 
-    currentDay = new Date().getDate(),
-    currentMonth = (new Date().getMonth()) + 1,
-    currentYear = new Date().getFullYear(),
-
-    discountedApplesPrice = ((applePrice - (applePrice * appleSalePercent / 100)) * appleCount).toFixed(),
-    discountedOrangesPrice = ((orangePrice - (orangePrice * orangeSalePercent / 100)) * orangeCount).toFixed(),
-    discountedKiwisPrice = kiwiPrice * (kiwiCount + (kiwiCount * kiwiCountryPercent / 100)).toFixed()
-    ;
-
-console.log(captionString.replaceAll(`≠`,`-`)
-            .replaceAll('<ВИВІД ПОТОЧНОГО ДНЯ>', currentDay)
-            .replaceAll('<ВИВІД ПОТОЧНОГО МІСЯЦЯ>', currentMonth)
-            .replaceAll('<ВИВІД ПОТОЧНОГО РОКУ>', currentYear)
-            );
-
-console.log(`Final price for ${appleCount} ${apple} = ${discountedApplesPrice} UAH`);
-console.log(`Final price for ${orangeCount} ${orange} = ${discountedOrangesPrice} UAH`);
-console.log(`Final price for ${kiwiCount} ${kiwi} = ${discountedKiwisPrice} UAH`);
-console.log(`Final price for all products = ${+discountedApplesPrice + +discountedOrangesPrice + +discountedKiwisPrice} UAH`);
+console.log(`Final price for all products = ${totalResult.toFixed()} UAH`);
