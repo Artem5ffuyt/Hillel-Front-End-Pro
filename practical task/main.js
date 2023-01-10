cc201ff62430681f9c3fb52aa99498b76ab9b40d
Artem Bachynskyi <amaz.good@gmail.com>
2023-01-10 22:10:33 +0200

    hom_work

--- practical task/main.js
+++ practical task/main.js
@@ -1,8 +1,157 @@
 
+// lesson_8
+// ----------------------------------------------------
+let arr = [2,2,2,2,2,33,33,31];
+let arr2 = [`snooppp`]
+for (let i; i<arr.length; i++){
+
+}
+console.log(arr2)
 
 
+// Copy of array
 
+// let arr = [10,20,30];
+// let arr_2 = [];
 
+// for(let i=0; i<arr.length; i++){
+//     arr_2[i] = arr[i];
+// }
+// console.log(arr_2);
+
+// arr_2[0] = `dog`;
+// console.log(arr, arr_2);
+
+// Copy of array: second
+// let arr = [10,20,30];
+// let arr_2 = [`lion`, `parrot`];
+
+// for(let i=0; i<arr.length; i++){
+//     arr_2[arr_2.length] = arr[i]; // 10
+// }
+// console.log(arr_2);
+// console.log(arr, arr_2);
+
+// ...
+
+// let arr = [10,20,30];
+// let arr_2 = [...arr]; // [10,20,30]
+
+// arr_2[0] = `dog`;
+// console.log(arr, arr_2);
+
+// let arr = [10,20,30];
+// let arr_2 = [`cat`, `dog`, ...arr ]; // [`cat`, `dog`, 10,20,30]
+// console.log(arr, arr_2);
+
+// Array methods
+//     push
+
+// let arr = [10,20];
+// arr.push(`cat`, true, 100);
+
+// let arrOfNewItems = [`cat`, true, 100];
+// for(let i=0; i<arrOfNewItems.length; i++){
+//     arr[arr.length] = arrOfNewItems[i];
+// }
+
+// console.log( arr.push(`cat`, true) ); // 4
+// console.log(arr);
+
+//     unshift
+
+// let arr = [10,20];
+// arr.unshift(`cat`, true, 100);
+// console.log( arr.unshift(`cat`, true, 100) ); // 5
+// console.log(arr);
+
+//     pop
+
+// let arr = [10,20,30,40];
+// // arr.pop();
+// console.log( arr.pop() ); // 40
+// console.log(arr);
+
+//     shift
+
+// let arr = [10,20,30,40];
+// arr.shift();
+// console.log(arr);
+
+//     reverse
+
+// let arr = [10,20,30,40,50];
+// arr.reverse();
+
+// console.log(arr);
+
+//     concat
+
+// let numbers = [10,20,30];
+// let animals = [`cat`, `dog`];
+// let users = [`student`, `lector`]
+
+// [10,20,30,`cat`, `dog`];
+
+// let newArr = numbers.concat(users, animals);
+
+// console.log(newArr);
+
+// let numbers = [10,20,30];
+// let animals = [`cat`, `dog`];
+// let users = [`student`, `lector`];
+
+// let finalArr = numbers
+//                 .concat(animals, users) // [10,20,30, `cat`, `dog`, `student`, `lector`]
+//                 .reverse(); // [10,20,30, `cat`, `dog`, `student`, `lector`]
+
+// finalArr.push(`HELLO`);
+// console.log(finalArr);
+
+//     slice
+
+// let arr = [10,20,30,40,50,60,70];
+// let arr_2 = arr.slice(2, -1);
+
+// console.log(arr, arr_2);
+
+//     splice
+
+// let arr = [10,20,30,40,50,60,70]; // [10,20, `cat`, `dog`, 10000, true,60,70]
+// let arr_2 = arr.splice(2, 0, `cat`, `dog`, 10000, true, false, `hello`);
+
+// console.log(arr, arr_2);
+
+//     join
+
+// let arr = [10,20,30,40];
+// let arrToString = arr.join(` `); // `10,20,30,40`
+
+// console.log(arr);
+// console.log(arrToString);
+
+// let arr = [10,20,30,40];
+// let LIs = arr.join(`</li><li>`);
+
+// console.log(LIs); // 10</li><li>20</li><li>30</li><li>40
+// document.write(`<ul><li>${LIs}</li></ul>`);
+
+//     split
+
+// let fullName = `Artem Kobzar`;
+// let userInfo = fullName.split(`m Ko`);
+
+// console.log(fullName);
+// console.log(userInfo);
+
+// let str = `first second third fourth`;
+
+// let strArr = str.match(` `);
+// console.log(strArr);
+
+// for(let i=0; i<strArr.length; i++){
+//     strArr[i] = strArr[i][0].toUpperCase() + strArr[i].slice(1).toLowerCase();
+// }
 
 // lessons_7
 // ------------------------------------------------------------------
@@ -13,32 +162,32 @@
 //    console.log(arr[i])
 //    }
 
-let arr = [10, -20, 40, 59, -37, 100, -15, 34];
-console.log(arr);
+// let arr = [10, -20, 40, 59, -37, 100, -15, 34];
+// console.log(arr);
 
-let minEl = maxEl = arr[0];
+// let minEl = maxEl = arr[0];
 
-for(let i=1; i<arr.length; i++){
-    if(arr[i] < minEl){
-        minEl = arr[i];
-    }
+// for(let i=1; i<arr.length; i++){
+//     if(arr[i] < minEl){
+//         minEl = arr[i];
+//     }
 
-    if(arr[i] > maxEl){
-        maxEl = arr[i];
-    }
-}
+//     if(arr[i] > maxEl){
+//         maxEl = arr[i];
+//     }
+// }
 
-console.log(minEl, arr.indexOf(minEl));
-console.log(maxEl, arr.indexOf(maxEl));
+// console.log(minEl, arr.indexOf(minEl));
+// console.log(maxEl, arr.indexOf(maxEl));
 
-let indexOfMin = arr.indexOf(minEl);
-let indexOfMax = arr.indexOf(maxEl);
+// let indexOfMin = arr.indexOf(minEl);
+// let indexOfMax = arr.indexOf(maxEl);
 
-let tempVal = arr[indexOfMin]; // -37
-arr[indexOfMin] = arr[indexOfMax];
-arr[indexOfMax] = tempVal;
+// let tempVal = arr[indexOfMin]; // -37
+// arr[indexOfMin] = arr[indexOfMax];
+// arr[indexOfMax] = tempVal;
 
-console.log(arr);
+// console.log(arr);
 
 
 // let enterFirstName = ``
